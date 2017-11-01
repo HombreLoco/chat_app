7879280ce13046bd22b22dd0abd8aae0046b1f4f
Jacques Begin <jacques.andre.begin@gmail.com>
2017-10-31 21:56:55 -0400

    Adds socket.io package. Establishes connection with the socket server.

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,49 @@
 import React, {Component} from 'react';
+import io from 'socket.io-client';
 
 class App extends Component {
+  constructor(props) {
+    super(props);
+    this.state = {};
+    this.socket = io("http://localhost:3030");
+  }
+
+  sendMessage = (chatSocket, chatMessage) => {
+    console.log("Currently in sendMessage function");
+    chatSocket.emit('textMessage', chatMessage);
+    // set value of current chat message to "" (empty string)
+  }
+
+  handleMessage = (chatMessage) => {
+    console.log("Currently in handleMessage function");
+    this.socket.emit('textMessage', "HELLO ALL CHAT");
+  }
+
+  componentDidMount() {
+    console.log("componentDidMount <App />");
+
+    this.handleMessage(this.socket);
+    // var socket = io.connect();
+    // this.socket = this.socket.connect();
+    this.socket.on('textMessage', function(chatMessage) {
+      console.log("Message Received: " + chatMessage);
+    });
+  }
+
   render() {
+    console.log("Rendering <App />");
     return (
-      <h1>Hello React :)</h1>
+      <div>
+        <ul id="messages"></ul>
+        <div>
+          <input id="m" autoComplete="off" />
+          <button id="send">Send</button>
+        </div>
+      </div>
     );
   }
+
+
 }
+
 export default App;
